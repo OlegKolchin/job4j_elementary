7435d5c6aafc74cb6b6202642b33b526a745928e
OlegKolchin <commshepard@gmail.com>
2020-07-01 21:47:32 +0500

    4.3.1. А существует ли треугольник? [#307670]

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -3,12 +3,12 @@ package ru.job4j.condition;
 public class Triangle {
 
     public static boolean exist(double ab, double ac, double bc) {
-        boolean condition = ab + ac > bc && ab + bc > ac && ac + bc > ab;
-        return condition;
+        return ab + ac > bc && ab + bc > ac && ac + bc > ab;
+
     }
 
     public static void main(String[] args) {
-        boolean triangle = Triangle.exist(2, 2, 2);
+        boolean triangle = Triangle.exist(2, 2, 5);
         System.out.println(triangle);
     }
 }
