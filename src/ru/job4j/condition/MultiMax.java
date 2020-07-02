befebd10d144df5f53efd6cdca309831511794d1
OlegKolchin <commshepard@gmail.com>
2020-07-02 08:16:08 +0500

    4.3.1. А существует ли треугольник? [#307670]

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -1,5 +1,19 @@
 package ru.job4j.condition;
 
 public class MultiMax {
+    public static int max(int first, int second, int third) {
+        boolean condition = first > second;
+        int result = condition ? first : second;
+        condition = result > third;
+        result = condition ? result : third;
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        int biggest = MultiMax.max(7, 8, 10);
+        System.out.println(biggest);
+    }
 
 }
