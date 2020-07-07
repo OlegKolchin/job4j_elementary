e36b161dc1df9d5601bb7f297bb9b9da287bf2a6
OlegKolchin <commshepard@gmail.com>
2020-07-07 22:16:19 +0500

    5.3. Построить шахматную доску в псевдографике. [#307767]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,11 +2,8 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        if (number == 1) {
-            prime = false;
+        boolean prime = number > 1;
 
-        }
         for (int divider = 2; divider < number; divider++) {
             if (number % divider == 0) {
                 prime = false;
