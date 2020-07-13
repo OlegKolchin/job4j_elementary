25afd622a69f53f21fdd291caf941569678c95ac
OlegKolchin <commshepard@gmail.com>
2020-07-13 15:41:34 +0500

    6.4.2. Слово заканчивается на ... [#307679]

--- src/ru/job4j/array/EqLast.java
+++ src/ru/job4j/array/EqLast.java
@@ -2,12 +2,6 @@ package ru.job4j.array;
 
 public class EqLast {
     public static boolean check(int[] left, int[] right) {
-        boolean rst = false;
-        if (left[left.length - 1] == right[right.length - 1]) {
-            rst = true;
-
-        }
-        return rst;
-
+        return left[left.length - 1] == right[right.length - 1];
     }
 }
